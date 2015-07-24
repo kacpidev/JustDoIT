68f7bae1b81798e7150c02d949c4f59a2b567ae4
Kacpi <kqpa93@gmail.com>
2015-07-24 19:19:09 +0200

    Basic project

---
+++ Source/JustDoIT/JustDoIT.Build.cs
@@ -0,0 +1,11 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class JustDoIT : ModuleRules
+{
+	public JustDoIT(TargetInfo Target)
+	{
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+	}
+}
---
+++ Source/JustDoIT/JustDoITHUD.h
@@ -0,0 +1,22 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+#pragma once 
+#include "GameFramework/HUD.h"
+#include "JustDoITHUD.generated.h"
+
+UCLASS()
+class AJustDoITHUD : public AHUD
+{
+	GENERATED_BODY()
+
+public:
+	AJustDoITHUD();
+
+	/** Primary draw call for the HUD */
+	virtual void DrawHUD() override;
+
+private:
+	/** Crosshair asset pointer */
+	class UTexture2D* CrosshairTex;
+
+};
+
